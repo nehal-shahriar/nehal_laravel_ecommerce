db24dff45114fce8236b39c26fef010b90dd1f90
nehal-shahriar <shahriarkhan.ndc@gmail.com>
2021-07-10 11:27:11 +0600

    commit 2

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,16 @@ Route::get('/shop', ShopComponent::class);
 Route::get('/cart',CartComponent::class);
 Route::get('/checkout',CheckoutComponent::class);
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+// Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//     return view('dashboard');
+// })->name('dashboard');
+//for User
+Route::middleware(['auth:sanctum', 'verified'])->group(function(){
+
+});
+
+//for Admin
+Route::middleware(['auth:sanctum', 'verified'])->group(function(){
+
+});
+
