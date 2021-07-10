0c01bb2ab4a13f7b2da6da1c68017249a2647b6d
nehal-shahriar <shahriarkhan.ndc@gmail.com>
2021-07-10 20:13:36 +0600

    commit 3

--- app/Http/Middleware/AuthAdmin.php
+++ app/Http/Middleware/AuthAdmin.php
@@ -16,6 +16,11 @@ class AuthAdmin
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        if (session('utype') === 'ADM') {
+            return $next($request);
+        } else {
+            session()->flush();
+            return redirect()->route('login');
+        }
     }
 }
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -58,20 +58,34 @@
 								@auth
 								@if(Auth::user()->utype === 'ADM')
 								<li class="menu-item menu-item-has-children parent">
-									<a title="My Account" href="#">My Account({{aUTH::USER()->NAME}})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+									<a title="My Account" href="#">My Account({{Auth::user()->name}})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
 									<ul class="submenu curency">
 										<li class="menu-item">
-											<a title="Dashboard" href="#">Dashboard</a>
+											<a title="Dashboard" href="{{ route('admin.dashboard') }}">Dashboard</a>
 										</li>
+										<li class="menu-item">
+											<a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+										</li>
+										<form id="logout-form" method="POST" action="{{ route('logout') }}">
+											@csrf
+
+										</form>
 									</ul>
 								</li>
 								@else
 								<li class="menu-item menu-item-has-children parent">
-									<a title="My Account" href="#">My Account({{aUTH::USER()->NAME}})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+									<a title="My Account" href="#">My Account({{Auth::user()->name}})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
 									<ul class="submenu curency">
 										<li class="menu-item">
-											<a title="Dashboard" href="#">Dashboard</a>
+											<a title="Dashboard" href="{{ route('user.dashboard') }}">Dashboard</a>
 										</li>
+										<li class="menu-item">
+											<a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+										</li>
+										<form id="logout-form" method="POST" action="{{ route('logout') }}">
+											@csrf
+
+										</form>
 									</ul>
 								</li>
 								@endif
