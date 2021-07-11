ff95153bdd35b6129f2662edd335dfb9837e50b4
nehal-shahriar <shahriarkhan.ndc@gmail.com>
2021-07-12 01:37:01 +0600

    commit 9

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -9,6 +9,13 @@ use Cart;
 
 class ShopComponent extends Component
 { 
+    public $sorting;
+    public $pagesize;
+
+    public function mount(){
+        $this->sorting="default";
+        $this->pagesize=12;
+    }
     public function store($product_id,$product_name,$product_price){
         Cart::add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
         session()->flash('success_message','Item added in cart');
@@ -17,7 +24,19 @@ class ShopComponent extends Component
     use WithPagination;
     public function render()
     {
-        $products = Product::paginate(12);
+        if($this->sorting=='date'){
+            $products = Product::orderBy('created_at','DESC')->paginate($this->pagesize);
+        }
+        elseif($this->sorting=='price'){
+            $products = Product::orderBy('regular_price','ASC')->paginate($this->pagesize);
+        }
+        elseif($this->sorting=='price-desc'){
+            $products = Product::orderBy('regular_price','DESC')->paginate($this->pagesize);
+        }
+        else{
+            $products = Product::paginate($this->pagesize);
+        }
+        
         return view('livewire.shop-component',['products'=>$products])->layout('layouts.base');
     }
 }
