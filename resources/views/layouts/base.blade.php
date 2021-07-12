a51f2ab399959cdf6ffaf7af8a479c38cdf87014
nehal-shahriar <shahriarkhan.ndc@gmail.com>
2021-07-12 22:14:56 +0600

    commit 12

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -64,6 +64,9 @@
 										<li class="menu-item">
 											<a title="Dashboard" href="{{ route('admin.dashboard') }}">Dashboard</a>
 										</li>
+										<li class="menu-item">
+											<a title="Categories" href="{{ route('admin.categories') }}">Categories</a>
+										</li>
 										<form id="logout-form" method="POST" action="{{ route('logout') }}">
 											@csrf
 											<li class="menu-item">
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Livewire\Admin\AdminAddCategoryComponent;
+use App\Http\Livewire\Admin\AdminCategoryComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CategoryComponent;
@@ -44,5 +46,7 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function(){
 //for Admin
 Route::middleware(['auth:sanctum', 'verified'])->group(function(){
     Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
+    Route::get('/admin/categories',AdminCategoryComponent::class)->name('admin.categories');
+    Route::get('/admin/category/add',AdminAddCategoryComponent::class)->name('admin.addcategories');
 });
 
