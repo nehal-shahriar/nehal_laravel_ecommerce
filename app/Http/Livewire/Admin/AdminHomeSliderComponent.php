39b5c9112b0aaa90eaa4c65943c7554668f9467d
nehal-shahriar <shahriarkhan.ndc@gmail.com>
2021-07-14 01:40:00 +0600

    commit 17

--- app/Http/Livewire/Admin/AdminHomeSliderComponent.php
+++ app/Http/Livewire/Admin/AdminHomeSliderComponent.php
@@ -4,12 +4,19 @@ namespace App\Http\Livewire\Admin;
 
 use App\Models\HomeSlider;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class AdminHomeSliderComponent extends Component
 {
+    use WithPagination;
+    public function deleteSlide($id){
+        $slider=HomeSlider::find($id);
+        $slider->delete();
+        session()->flash('message','Slide has been deleted successfully');
+    }
     public function render()
     {
-        $sliders=HomeSlider::all();
+        $sliders=HomeSlider::paginate(10);
         return view('livewire.admin.admin-home-slider-component',['sliders'=>$sliders])->layout('layouts.base');
     }
 }
